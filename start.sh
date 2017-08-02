06f3ab11c5499464ac2ee9edc85a2183499cb124
Stefan Dirix <sdirix@eclipsesource.com>
2017-08-02 16:49:02 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ EXTRA_ARGS=--fast
 
 working_dir=$(pwd)
 che_version=$(cat pom.xml  | grep "<.*che.version\>" | grep -o  "[0-9].*[0.9].*[0-9]")
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
 docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock \
-	-v $working_dir/:/repo \
-	-v $working_dir/data:/data \
-	eclipse/che:$che_version \
-	start --debug $EXTRA_ARGS
+       -v $working_dir/assembly/assembly-main/target/eclipse-che-1.0.0-SNAPSHOT/eclipse-che-1.0.0-SNAPSHOT:/assembly \
+       -v $working_dir/che-data/$DATE/:/data \
+       eclipse/che-cli:$che_version start --debug $EXTRA_ARGS
